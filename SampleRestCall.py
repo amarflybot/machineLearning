4f90fae2e94fe91e722f58db4684f7c5b21e20b5
Amarendra Kumar <amar.arawat@gmail.com>
2016-12-19 22:19:59 +0530

    Change in Rest Calls

--- SampleRestCall.py
+++ SampleRestCall.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def hello_world():
-    return 'Hello Amar!'
-
+    if request.method == 'POST':
+        if request.form['user']:
+            return 'Hello User: '+request.form['user']
+    else: return 'Hello Amar from GET!'
 
 if __name__ == '__main__':
     app.run()
