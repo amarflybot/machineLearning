00f367f49045e2bb90bde2b90fa2815df4ea9f01
amarraja123 <amar.arawat@gmail.com>
2016-12-27 23:58:04 +0530

    change for k means

--- clustering/k_means_clustering.py
+++ clustering/k_means_clustering.py
@@ -1,4 +1,3 @@
-import csv
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 
@@ -8,7 +7,7 @@ def main():
     lines = [line.split("\t") for line in lines if len(line.split("\t")) == 2 and line.split("\t")[1] != '']
     train_documents = [line[0] for line in lines]
     tfid_vectorizer = TfidfVectorizer(max_df=0.5, min_df=2, stop_words='english')
-    train_documents = tfid_vectorizer.fit(train_documents)
+    train_documents = tfid_vectorizer.fit_transform(train_documents)
     k_means = KMeans(n_clusters=3, init='k-means++', max_iter=100, n_init=1, verbose=True)
     k_means.fit(train_documents)
 
